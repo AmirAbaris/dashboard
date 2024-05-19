204b8a6e561f16300b20a90495cb8deb834217cd
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-05-19 23:31:48 +0330

    [#140-UPDATE] added a side bar item and the data it needs

--- src/app/modules/dashboard.module.ts
+++ src/app/modules/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardMainComponent } from '../components/dashboard/dashboard-main/dashboard-main.component';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { NgOptimizedImage } from '@angular/common';
 
 const routes: Routes = [
@@ -11,6 +12,6 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [DashboardMainComponent],
-  imports: [RouterModule.forChild(routes), NgOptimizedImage, MatDividerModule]
+  imports: [RouterModule.forChild(routes), NgOptimizedImage, MatDividerModule, MatIconModule]
 })
 export class DashboardModule { }
