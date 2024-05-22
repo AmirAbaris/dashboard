025b1b80f68118b1680f80bd62f240bac40b8829
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-05-22 19:08:31 +0330

    [#142-update] Fixed a component path

--- src/app/modules/profile.module.ts
+++ src/app/modules/profile.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileMainComponent } from '../components/dashboard/profile/profile-main/profile-main.component';
-import { ProfileOverviewComponent } from '../components/dashboard/profile-overview/profile-overview.component';
+import { ProfileOverviewComponent } from '../components/dashboard/profile/profile-overview/profile-overview.component';
 import { ProjectItemComponent } from '../components/dashboard/profile/project-item/project-item.component';
 
 const routes: Routes = [
