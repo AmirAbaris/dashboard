fb36ae811766ec4d99521f1c82c8264548fc9383
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-05-25 17:17:41 +0330

    [#142-REFACTOR] updated profile path in directories

--- src/app/modules/profile.module.ts
+++ src/app/modules/profile.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { ProfileMainComponent } from '../components/dashboard/profile/profile-main/profile-main.component';
-import { ProfileOverviewComponent } from '../components/dashboard/profile/profile-overview/profile-overview.component';
-import { ProjectItemComponent } from '../components/dashboard/profile/project-item/project-item.component';
-import { ProjectComponent } from '../components/dashboard/profile/project/project.component';
+import { ProfileMainComponent } from '../components/profile/profile-main/profile-main.component';
+import { ProfileOverviewComponent } from '../components/profile/profile-overview/profile-overview.component';
+import { ProjectItemComponent } from '../components/profile/project-item/project-item.component';
+import { ProjectComponent } from '../components/profile/project/project.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'profile', pathMatch: 'full' },
