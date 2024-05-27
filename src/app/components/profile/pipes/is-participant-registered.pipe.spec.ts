7227ac0fe703c1523d694c9b5c0a2b53aea3dcf7
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-05-28 00:54:46 +0330

    [#142-UPDATE] removed the pipes

--- src/app/components/profile/pipes/is-participant-registered.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IsParticipantRegisteredPipe } from './is-participant-registered.pipe';
-
-describe('IsParticipantRegisteredPipe', () => {
-  it('create an instance', () => {
-    const pipe = new IsParticipantRegisteredPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/components/profile/pipes/participant-count.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ParticipantCountPipe } from './participant-count.pipe';
-
-describe('ParticipantCountPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ParticipantCountPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
