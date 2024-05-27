ed67f26e87591b71dbdd5664a19af137ae405592
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-05-27 23:13:58 +0330

    [#142-REFACTOR] refactored the component

--- src/app/components/profile/profile-main/profile-main.component.ts
+++ src/app/components/profile/profile-main/profile-main.component.ts
@@ -1,59 +1,10 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { ProjectItemModel } from '../models/project-item.model';
-import { ParticipantModel } from '../models/participant.model';
-import { forkJoin } from 'rxjs';
-import { UserService } from '../../../services/user.service';
-import { ProjectInputCaptionModel } from '../models/caption-models/project-input.caption.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-profile-main',
   templateUrl: './profile-main.component.html',
   styleUrl: './profile-main.component.scss'
 })
-export class ProfileMainComponent implements OnInit {
-  //#region Properties
-  private readonly _translateService = inject(TranslateService);
-  private readonly _userService = inject(UserService);
+export class ProfileMainComponent {
 
-  public participants: ParticipantModel[] | undefined;
-  public projectItems: ProjectItemModel[] | undefined;
-
-  public projectInputCaption: ProjectInputCaptionModel | undefined;
-  private readonly _profileCaptionPath = {
-    projectCaption: 'profile.project',
-    projectItemCaption: 'profile.project-item'
-  }
-  //#endregion
-
-  //#region Lifecycle methods
-  public ngOnInit(): void {
-    this._getCaptions();
-    this._getData();
-  }
-  //#endregion
-
-  //#region Main logic methods
-  private _getCaptions(): void {
-    const projectCaption = this._translateService.get(this._profileCaptionPath.projectCaption);
-    const projectItemCaption = this._translateService.get(this._profileCaptionPath.projectItemCaption);
-
-    forkJoin([projectCaption, projectItemCaption]).subscribe(([projectCaptionData, projectItemCaptionData]) => {
-      this.projectInputCaption = {
-        projectCaption: projectCaptionData,
-        projectItemCaption: projectItemCaptionData
-      }
-    });
-  }
-
-  private _getData(): void {
-    const projectItem = this._userService.getProjectItem();
-    const participant = this._userService.getParticipant();
-
-    forkJoin([projectItem, participant]).subscribe(([projectItemData, participantData]) => {
-      this.projectItems = projectItemData;
-      this.participants = participantData;
-    });
-  }
-  //#endregion
 }
--- src/app/components/profile/profile-project/profile-project.component.ts
+++ src/app/components/profile/profile-project/profile-project.component.ts
@@ -1,17 +1,59 @@
-import { Component, input } from '@angular/core';
+import { Component, OnInit, inject, input } from '@angular/core';
 import { ParticipantModel } from '../models/participant.model';
 import { ProjectItemModel } from '../models/project-item.model';
 import { ProjectInputCaptionModel } from '../models/caption-models/project-input.caption.model';
+import { TranslateService } from '@ngx-translate/core';
+import { forkJoin } from 'rxjs';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-project',
   templateUrl: './profile-project.component.html',
   styleUrl: './profile-project.component.scss'
 })
-export class ProfileProjectComponent {
+export class ProfileProjectComponent implements OnInit {
   //#region Properties
-  public caption = input.required<ProjectInputCaptionModel>();
-  public projectItems = input.required<ProjectItemModel[]>();
-  public participants = input.required<ParticipantModel[]>();
+  private readonly _translateService = inject(TranslateService);
+  private readonly _userService = inject(UserService);
+
+  public participants: ParticipantModel[] | undefined;
+  public projectItems: ProjectItemModel[] | undefined;
+
+  public caption: ProjectInputCaptionModel | undefined;
+  private readonly _projectCaptionPath = {
+    projectCaption: 'profile.project',
+    projectItemCaption: 'profile.project-item'
+  }
+  //#endregion
+
+  //#region Lifecycle methods
+  public ngOnInit(): void {
+    this._getCaptions();
+    this._getData();
+  }
+  //#endregion
+
+  //#region Main logic methods
+  private _getCaptions(): void {
+    const projectCaption = this._translateService.get(this._projectCaptionPath.projectCaption);
+    const projectItemCaption = this._translateService.get(this._projectCaptionPath.projectItemCaption);
+
+    forkJoin([projectCaption, projectItemCaption]).subscribe(([projectCaptionData, projectItemCaptionData]) => {
+      this.caption = {
+        projectCaption: projectCaptionData,
+        projectItemCaption: projectItemCaptionData
+      }
+    });
+  }
+
+  private _getData(): void {
+    const projectItem = this._userService.getProjectItem();
+    const participant = this._userService.getParticipant();
+
+    forkJoin([projectItem, participant]).subscribe(([projectItemData, participantData]) => {
+      this.projectItems = projectItemData;
+      this.participants = participantData;
+    });
+  }
   //#endregion
 }
