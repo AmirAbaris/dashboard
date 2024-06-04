576e7b8442aaee015b07863b96ab27a0150b0676
Abaris <118467205+AmirAbaris@users.noreply.github.com>
2024-06-04 15:15:01 +0330

    [#146-UPDATE] added output and model for slide toggles

---
+++ src/app/components/profile-overview-general/models/toggle-change-data.model.ts
@@ -0,0 +1,4 @@
+export interface ToggleChangeDataModel {
+    name: string;
+    value: boolean;
+}
--- src/app/components/profile-overview-general/profile-overview-platform-setting/profile-overview-platform-setting.component.ts
+++ src/app/components/profile-overview-general/profile-overview-platform-setting/profile-overview-platform-setting.component.ts
@@ -1,5 +1,7 @@
-import { Component, input } from '@angular/core';
+import { ToggleChangeDataModel } from '../models/toggle-change-data.model';
+import { Component, input, output } from '@angular/core';
 import { ProfileOverviewPlatformSettingCaptionModel } from '../models/caption-models/profile-overview-platform-setting.caption.model';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-profile-overview-platform-setting',
@@ -9,5 +11,46 @@ import { ProfileOverviewPlatformSettingCaptionModel } from '../models/caption-mo
 export class ProfileOverviewPlatformSettingComponent {
   //#region Properties
   public caption = input.required<ProfileOverviewPlatformSettingCaptionModel>();
+  public slideToggleEvent = output<ToggleChangeDataModel>();
+
+  public emailWhenFollows = false;
+  public emailWhenAnswers = false;
+  public emailWhenMentions = false;
+  public newLaunchesAndProjects = false;
+  public monthlyProductUpdates = false;
+  public subscribeToNewsletter = false;
+  public readonly toggleNames = {
+    emailWhenFollows: 'emailWhenFollows',
+    emailWhenAnswers: 'emailWhenAnswers',
+    emailWhenMentions: 'emailWhenMentions',
+    newLaunchesAndProjects: 'newLaunchesAndProjects',
+    monthlyProductUpdates: 'monthlyProductUpdates',
+    subscribeToNewsletter: 'subscribeToNewsletter'
+  };
+
+  //#endregion
+
+  //#region Handler methods
+  /**
+   * Handles the change event of a slide toggle.
+   * It updates the value of the toggle in the component
+   * and emits an event with the new value.
+   *
+   * @param {MatSlideToggleChange} event - The event object containing information about the toggle change.
+   * @param {string} toggleName - The name of the toggle.
+   * @return {void} This function does not return a value.
+   */
+  public slideToggleEventHandler(event: MatSlideToggleChange, toggleName: string): void {
+    // Create a new object with the name and value of the slide toggle
+    const toggleChangeValue: ToggleChangeDataModel = {
+      name: toggleName,
+      value: event.checked
+    }
+
+    console.log('slide change value:', toggleChangeValue);
+
+    // Emit the slideToggleEvent with the new value
+    this.slideToggleEvent.emit(toggleChangeValue);
+  }
   //#endregion
 }
